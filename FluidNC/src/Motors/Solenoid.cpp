dbb038cbe208337807c03e8c06a339ee2471999e
Bart Dring <bdring@buildlog.net>
2021-09-23 08:20:32 -0500

    WIP Setup Class don't use...no code yet.

---
+++ FluidNC/src/Motors/Solenoid.cpp
@@ -0,0 +1,126 @@
+// Copyright (c) 2021 -	Bart Dring
+// Use of this source code is governed by a GPLv3 license that can be found in the LICENSE file.
+
+/*
+    This lets an solenoid be used like any other motor. 
+
+    The solenoid will be in one position above machine 0 and the other below machine 0
+
+    It will do an initial pull power, hold for a while, then reduce to a hold power
+    It will disable like all other axes.
+
+
+
+*/
+
+#include "Solenoid.h"
+
+#include "../Machine/MachineConfig.h"
+#include "../System.h"  // mpos_to_steps() etc
+#include "../Pins/LedcPin.h"
+#include "../Pin.h"
+#include "../Limits.h"  // limitsMaxPosition
+#include "../NutsBolts.h"
+
+namespace MotorDrivers {
+
+    void Solenoid::init() {
+        if (_output_pin.undefined()) {
+            log_warn("    Solenoid disabled: No output pin");
+            _has_errors = true;
+            return;  // We cannot continue without the output pin
+        }
+
+        _axis_index = axis_index();
+
+        read_settings();
+        config_message();
+
+        _pwm_chan_num     = ledcInit(_output_pin, -1, double(_pwm_freq), calc_pwm_precision(_pwm_freq));  // Allocate a channel
+        _current_pwm_duty = 0;
+
+        _disabled = true;
+    }
+
+    /*
+		Calculate the highest precision of a PWM based on the frequency in bits
+
+		80,000,000 / freq = period
+		determine the highest precision where (1 << precision) < period
+	*/
+    uint8_t Solenoid::calc_pwm_precision(uint32_t freq) {
+        uint8_t precision = 0;
+        if (freq == 0) {
+            return precision;
+        }
+
+        // increase the precision (bits) until it exceeds allow by frequency the max or is 16
+        while ((1u << precision) < uint32_t(80000000 / freq) && precision <= 16) {
+            precision++;
+        }
+
+        return precision - 1;
+    }
+
+    void Solenoid::config_message() { log_info("    Solenoid Pin:" << _output_pin.name() << ")"); }
+
+    void Solenoid::_write_pwm(uint32_t duty) {
+        // to prevent excessive calls to ledcWrite, make sure duty has changed
+        if (duty == _current_pwm_duty) {
+            return;
+        }
+
+        _current_pwm_duty = duty;
+        ledcSetDuty(_pwm_chan_num, duty);
+    }
+
+    // sets the PWM to zero. This allows most servos to be manually moved
+    void IRAM_ATTR Solenoid::set_disable(bool disable) {
+        //log_info("Set dsbl " << disable);
+        if (_has_errors)
+            return;
+
+        _disabled = disable;
+        if (_disabled) {
+            _write_pwm(0);
+        }
+    }
+
+    // Homing justs sets the new system position and the servo will move there
+    bool Solenoid::set_homing_mode(bool isHoming) {
+        //log_info("Servo homing:" << isHoming);
+        if (_has_errors)
+            return false;
+
+        if (isHoming) {
+            auto axis                = config->_axes->_axis[_axis_index];
+            motor_steps[_axis_index] = mpos_to_steps(axis->_homing->_mpos, _axis_index);
+            _disabled = false;
+            set_location();  // force the PWM to update now
+        }
+        return false;  // Cannot be homed in the conventional way
+    }
+
+    void IRAM_ATTR Solenoid::step() {
+        
+    }
+
+    void Solenoid::set_location() {
+        if (_disabled || _has_errors) {
+            return;
+        }
+
+        uint32_t solenoid_pulse_len;
+
+        read_settings();
+
+        _write_pwm(solenoid_pulse_len);
+    }
+
+    void Solenoid::read_settings() {}
+
+    // Configuration registration
+    namespace {
+        MotorFactory::InstanceBuilder<Solenoid> registration("solenoid");
+    }
+}
--- FluidNC/src/Motors/Solenoid.h
+++ FluidNC/src/Motors/Solenoid.h
@@ -1,20 +1,58 @@
+// Copyright (c) 2020 -	Bart Dring
+// Use of this source code is governed by a GPLv3 license that can be found in the LICENSE file.
+
 #pragma once
 
-#include "RcServo.h"
+#include "MotorDriver.h"
 
 namespace MotorDrivers {
-    class Solenoid : public RcServo {
-    public:
-        Solenoid();
-        Solenoid(size_t axis_index, gpio_num_t pwm_pin, float transition_poiont);
-        void           set_location();
-        void           update() override;
-        void           init() override;
-        void IRAM_ATTR set_disable(bool disable) override;
-
-        float _transition_point;
-
+    class Solenoid : public MotorDriver {
     protected:
         void config_message() override;
+
+        void set_location();
+
+        uint8_t calc_pwm_precision(uint32_t freq);
+
+        Pin      _output_pin;
+        uint32_t _pwm_freq = SERVO_PWM_FREQ_DEFAULT;  // 50 Hz
+        uint8_t  _pwm_chan_num;
+        uint32_t _current_pwm_duty;
+        float _peak_duty;
+        uint32_t _peak_ms;
+        float _hold_duty; 
+
+        bool _disabled;
+
+        int _axis_index = -1;
+
+        bool _has_errors = false;
+
+    public:
+        Solenoid() = default;
+
+        // Overrides for inherited methods
+        void init() override;
+        void read_settings() override;
+        bool set_homing_mode(bool isHoming) override;
+        void set_disable(bool disable) override;
+        //void update() override;
+        void step() override;
+
+        void _write_pwm(uint32_t duty);
+
+        // Configuration handlers:
+        void group(Configuration::HandlerBase& handler) override {
+            handler.item("output_pin", _output_pin);            
+            handler.item("pwm_freq", _pwm_freq);
+            handler.item("peak_duty", _peak_duty);            
+            handler.item("peak_ms", _peak_ms);
+            handler.item("peak_ms", _hold_duty);
+
+        }
+
+        // Name of the configurable. Must match the name registered in the cpp file.
+        const char* name() const override { return "solenoid"; }
     };
 }
+
